6856ef526243842fa0eff9a8bccecf1f35695362
meng-Shiun <lin.mengshiun@gmail.com>
2018-07-02 17:11:57 +0200

    Add onChange event to Book

--- src/Book.js
+++ src/Book.js
@@ -1,26 +1,33 @@
 import React from 'react'
 
-const Book = (props) => {
-  return (
-    <li>
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${props.cover})` }}></div>
-          <div className="book-shelf-changer">
-            <select>
-              <option value="move" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
-        </div>
-        <div className="book-title">{props.title}</div>
-        <div className="book-authors">{props.authors}</div>
-      </div>
-    </li>
-    )
+class Book extends React.Component {
+
+  onChangeBookShelf = (e) => {
+    console.log('Move', this.props.title,  'To bookshelf:', e.target.value)
   }
 
-  export default Book
+  render() {
+    return (
+      <li>
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${this.props.cover})` }}></div>
+            <div className="book-shelf-changer">
+              <select onChange={this.onChangeBookShelf}>
+                <option value="move" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+          </div>
+          <div className="book-title">{this.props.title}</div>
+          <div className="book-authors">{this.props.authors}</div>
+        </div>
+      </li>
+    )
+  }
+}
+
+export default Book
