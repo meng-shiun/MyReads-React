9d4d6b3a5e512ecf515ebf1979cd4b6207a3720c
meng-Shiun <lin.mengshiun@gmail.com>
2018-07-03 20:14:01 +0200

    Cover ListBooks to Functional Components

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import BookShelf from './BookShelf'
 
-class ListBooks extends React.Component {
-  render() {
+const ListBooks = (props) => {
+
     const currentlyReadingBooks = []
     const wantToReadBooks = []
     const readBooks = []
 
-    const { books, handleShelfChange } = this.props
+    const { books, handleShelfChange } = props
 
     books.forEach(book => {
       if (book.shelf === 'currentlyReading') {
@@ -24,24 +24,20 @@ class ListBooks extends React.Component {
     return (
       <div>
         <div className='list-books'>
-
           <div className='list-books-title'>
             <h1>MyReads</h1>
           </div>
-
           <div className='list-books-content'>
             <BookShelf title='Currently Reading' books={currentlyReadingBooks} onShelfChange={handleShelfChange}/>
             <BookShelf title='Want to Read' books={wantToReadBooks} onShelfChange={handleShelfChange}/>
             <BookShelf title='Read' books={readBooks} onShelfChange={handleShelfChange}/>
           </div>
-
           <div className='open-search'>
             <a>Add a book</a>
           </div>
         </div>
       </div>
     )
-  }
 }
 
 export default ListBooks
